ceffd29f10103f3dc94658e08e5e6db07ec724cd
NicolaiAA <45482830+NicolaiAA@users.noreply.github.com>
2019-10-25 13:01:01 +0200

    Add files via upload

---
+++ MockEksamen/Webpack-template/src/js/index.ts
@@ -0,0 +1,8 @@
+function Beregning() : void{
+    let Indskrevetværdi:HTMLInputElement=<HTMLInputElement>document.getElementById("pris indskrevet");
+    let indskrevetværdiifelt:number=parseInt(Indskrevetværdi.value);
+    let vlagtbilelement:HTMLSelectElement=<HTMLSelectElement>document.getElementById("valgte")
+    let valgtbil:string=vlagtbilelement.value;
+
+
+}
